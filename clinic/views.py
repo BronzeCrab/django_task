00c2b3feef047eb134bdd7a05d1309b7d957b7ba
Alexey Ustinnikov <gr4k@bk.ru>
2015-09-19 11:11:12 +0300

    fixed time checking and loggin to allapps

--- clinic/views.py
+++ clinic/views.py
@@ -4,12 +4,13 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 
 from .models import Appointment
+import datetime
 
 # Create your views here.
 
 
 class AppointmentForm(forms.Form):
-	# just dummy docs names
+        # just dummy docs names
     VIKTOROVA = 'Viktorova'
     IVANOVA = 'Ivanova'
     DOCS_NAMES = (
@@ -35,14 +36,19 @@ def index(request):
 
 
 def all_apps(request):
-    appointments = Appointment.objects.all()
-    return render(
-        request,
-        'clinic/all_apps.html',
-        {
-            'appointments': appointments,
-        }
-    )
+    if request.user.is_authenticated():
+        appointments = Appointment.objects.all()
+        return render(
+            request,
+            'clinic/all_apps.html',
+            {
+                'appointments': appointments,
+            }
+        )
+    else:
+        messages.add_message(request, messages.ERROR,
+                             'You cant accesss')
+        return redirect('home')
 
 
 def newappointment(request):
@@ -50,6 +56,22 @@ def newappointment(request):
     a.doc_name = request.POST['doc_name']
     a.visit_date = request.POST['visit_date']
     a.visit_time = request.POST['visit_time']
+    # first of all i must check if somebody
+    # want to see doctor today but that time has
+    # already passed, so it's impossible
+
+    # what time is now
+    now = datetime.datetime.now()
+    time_now = now.strftime("%H:%M")
+    date_now = now.strftime("%Y-%m-%d")
+    # check if current time is alredy passed
+    if a.visit_date == date_now and (int(time_now[0:2]) >
+        int(a.visit_time[0:2]) or (int(time_now[0:2]) == int(a.visit_time[0:2])
+        and int(time_now[3:5]) > int(a.visit_time[3:5]))):
+        messages.add_message(request, messages.ERROR,
+                             'Please specify another time, because, \
+                                 that time is already passed')
+        return redirect('home')
 
     # === olf stuff, done that using js pickers ===
     # if (int(a.visit_time[0:2]) > 17 or
